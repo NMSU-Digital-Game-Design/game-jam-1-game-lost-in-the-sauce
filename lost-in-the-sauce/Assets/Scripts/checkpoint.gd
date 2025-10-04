52fb4aeec8fcb134721c5940ff21394383e814df
uziwils <uzi23cx@gmail.com>
2025-10-03 23:52:48 -0600

    updating file pathing and adding checkpoints/deathzone scenes and scripts

---
+++ lost-in-the-sauce/Assets/Scripts/checkpoint.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+var checkpoint_manager
+
+# Called when the node enters the scene tree for the first time.
+# once again, make sure to check how the files are set up and run off that
+func _ready() -> void:
+	checkpoint_manager = get_parent().get_parent().get_node("checkpointManager")
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# this updates wehre the respawn point will be set at. goes off the instances established in the
+# checkpoint scene.
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		checkpoint_manager.lastLocation = $respawn_point.global_position
