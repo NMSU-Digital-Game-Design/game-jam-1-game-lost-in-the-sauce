4e82be75bba83c466bec8c0970de7f27388289b5
MoreMoople <jacksone003@gmail.com>
2025-10-05 18:24:13 -0400

    meatball and mushroom monsters

--- lost-in-the-sauce/Monsters/mushroom_enemy.gd
+++ lost-in-the-sauce/Monsters/mushroom_enemy.gd
@@ -5,22 +5,15 @@ extends Node2D
 var path_ended = false
 
 # speed in px/sec
-@export var speed = 100
+@export var speed = 80
 
 func _ready():
 	show()
-	sprite.animation = "roll"
+	sprite.animation = "hop"
 	sprite.play()
 
 func _process(delta: float) -> void:
 	path_follow.progress += speed * delta
-	if path_follow.progress_ratio == 0.5:
-		if path_ended == false:
-			sprite.animation = "roll"
-			path_ended = true
-		else:
-			sprite.animation = "rollReverse"
-			path_ended = false
 
 func _on_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	hide()
