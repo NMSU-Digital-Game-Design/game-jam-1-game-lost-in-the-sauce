9378f2fd9c2db762bd3067f8d745fab673caba17
uziwils <uzi23cx@gmail.com>
2025-10-05 21:16:46 -0600

    organization with the new assets

--- lost-in-the-sauce/Assets/Monsters/enemy_node.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var path_follow : PathFollow2D = $Path2D/PathFollow2D
-
-# speed in px/sec
-@export var speed = 100
-
-func _process(delta: float) -> void:
-	path_follow.progress += speed * delta
